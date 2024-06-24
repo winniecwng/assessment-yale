913c40acef0ede0be9f20f36a0ab67d45526c385
Winnie <winnieng3210@gmail.com>
2024-06-24 12:02:36 -0400

    post api to fetch uids

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI, status, Depends, HTTPException
+from fastapi import FastAPI, status, Depends, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated, Optional
-# from sqlalchemy.orm import Session
+import requests
 
 app = FastAPI()
+
+baseUrl = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+
+@app.post("/search")
+async def fetch_publication_uids(db:str, term: str):
+
+    
+
+    fullUrl = f"{baseUrl}/esearch.fcgi?db={db}&term={term}&retmode=JSON"
+
+    try:
+        response = requests.get(fullUrl)
+        response.raise_for_status()  
+        return response.json()  
+        
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Error connecting to NCBI API: {str(e)}")
