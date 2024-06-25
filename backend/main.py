d00518090a4c1060e31cac885496141b6ad17187
Winnie <winnieng3210@gmail.com>
2024-06-25 12:16:05 -0400

    add api key for more request

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import requests
@@ -5,6 +8,9 @@ from pydantic import BaseModel
 from xmljson import badgerfish as bf
 from xml.etree.ElementTree import fromstring
 import json
+import os
+
+API_KEY = os.getenv("API_KEY")
 
 app = FastAPI()
 
@@ -35,14 +41,14 @@ class PubInfoRequest(BaseModel):
 def search_publication_uids(request_data: TermRequest):
     term = request_data.term
     start = str(request_data.start)
-    firstUrl = f"{baseUrl}/esearch.fcgi?db=pubmed&term={term}&retstart={start}&retmax=5&retmode=JSON"
+    firstUrl = f"{baseUrl}/esearch.fcgi?db=pubmed&term={term}&retstart={start}&retmax=3&retmode=JSON"
     
     
     try:
         response = requests.get(firstUrl)
         response.raise_for_status()  
         uidList = response.json()["esearchresult"]["idlist"]
-        # print(uidList)
+
         uidsInfo = []
         for uid in uidList:
             uidInfo = fetch_publication_info(uid)
@@ -52,8 +58,7 @@ def search_publication_uids(request_data: TermRequest):
             pub_title = baseData["Article"]["ArticleTitle"]["$"]
             pub_year = baseData["Article"]["Journal"]["JournalIssue"]["PubDate"]["Year"]["$"]
 
-            print(pub_title)
-            print(pub_year)
+
             uidsInfo.append({
                 "pub_title": pub_title,
                 "pub_year": pub_year,
@@ -67,7 +72,7 @@ def search_publication_uids(request_data: TermRequest):
 
 @app.get("/fetch-info/{pub_id}")
 def fetch_publication_info(pub_id: str):
-    firstUrl = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={pub_id}&retmode=xml"
+    firstUrl = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={pub_id}&retmode=xml&api_key={API_KEY}"
     
     try:
         response = requests.get(firstUrl)
